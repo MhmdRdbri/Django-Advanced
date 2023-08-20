086e8d5dad2c9987b2c0c881a085290e6c6c8bfa
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-21 00:15:59 +0330

    update urls.py, urls.py and urls.py

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = 'api_v1'
+
+urlpatterns = [
+]
+
--- core/core/urls.py
+++ core/core/urls.py
@@ -23,6 +23,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('blog/', include('blog.urls', namespace='blog')),
     path('api-auth/', include('rest_framework.urls')),
     path('api-docs/', include_docs_urls(title='ALL API')),
