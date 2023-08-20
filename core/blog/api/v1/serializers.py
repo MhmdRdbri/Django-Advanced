b239df6d7991e68b721751799fa4605da9e705fa
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-20 10:27:20 +0330

    update serializers.py and representation edit

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from blog.models import *
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
 class postSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
@@ -12,8 +17,9 @@ class postSerializer(serializers.ModelSerializer):
     def get_absolute_url(self,obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
+    
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['category'] = CategorySerializer(instance.category).data
+        rep.pop('snippet', None)
+        return rep
