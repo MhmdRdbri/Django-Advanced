f49826760919e97c01247c03c702f27f418187e0
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-21 15:13:48 +0330

    update views.py

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,11 +1,18 @@
 from rest_framework import generics
+from rest_framework import status
 from .serializers import RegistrationSerializer
+from rest_framework.response import Response
 
 
 class RegistrationApiView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
     
     def post(self, request, *args, **kwargs):
-        
-        return super().post(request, *args, **kwargs)
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                'email':serializer.validated_data['email'],
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
     
