543bfc2e443e107ff822fb235c2d4610a30db608
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-22 20:53:33 +0330

    update admin.py, users.py and settings.py

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -5,8 +5,8 @@ from .models import *
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'is_superuser', 'is_active')
-    list_filter = ('email', 'is_superuser', 'is_active')
+    list_display = ('email', 'is_superuser', 'is_active', 'is_verified')
+    list_filter = ('email', 'is_superuser', 'is_active', 'is_verified')
     searching_fields = ('email',)
     ordering = ('email',)
     fieldsets = (
@@ -17,7 +17,7 @@ class CustomUserAdmin(UserAdmin):
         }),
         ('Permissions', {
             'fields': (
-                'is_superuser', 'is_active', 'is_staff'
+                'is_superuser', 'is_active', 'is_staff', 'is_verified'
             ),
         }),
         ('Group Permissions', {
@@ -34,7 +34,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser')
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser', 'is_verified')
         }
          ),
     )
