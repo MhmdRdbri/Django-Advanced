38aef71be68738b3c93ffb7bc403cf46cc17d6bd
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-22 09:51:27 +0330

    update urls.py and views.py

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -2,12 +2,12 @@ from django.urls import include, path
 from . import views
 from rest_framework.routers import DefaultRouter
 from . import views
-from rest_framework.authtoken.views import ObtainAuthToken
+# from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = 'api_v1'
 
 urlpatterns = [
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
-    path('tokrn/login', ObtainAuthToken.as_view(), name='token-login'),
+    path('tokrn/login/', view.CustomAuthToken.as_view(), name='token-login'),
 ]
 
