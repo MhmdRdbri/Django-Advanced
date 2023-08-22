ae684b0e00e382d8468b382e02e69c0e6337bb4f
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-22 09:15:05 +0330

    Token added to porject

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -2,10 +2,12 @@ from django.urls import include, path
 from . import views
 from rest_framework.routers import DefaultRouter
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = 'api_v1'
 
 urlpatterns = [
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
+    path('tokrn/login', ObtainAuthToken.as_view(), name='token-login'),
 ]
 
