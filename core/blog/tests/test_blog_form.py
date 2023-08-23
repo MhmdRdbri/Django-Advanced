a1e22ce6e28c8b758e7bc05960fab760a3f22229
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-23 12:55:57 +0330

    test: update test_blog_form.py and test_blog_urls.py

---
+++ core/blog/tests/test_blog_form.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from django.test import SimpleTestCase, TestCase
+from ..models import Category
+from ..forms import *
+
+class TestPostForm(TestCase):
+    
+    def test_post_form_with_valid_data(self):
+        category_obj = Category.objects.create(name='Hello')
+        form = PostForm(data={
+            "title": "Test",
+            "content": "Test Form",
+            "status": True,
+            "category":category_obj,
+            "published_at": datetime.now()
+        })
+        self.assertTrue(form.is_valid())
