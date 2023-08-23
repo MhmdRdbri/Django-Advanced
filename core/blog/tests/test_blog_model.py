5c71469ab7d34a0874f7a3476af97ca89b55b60f
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-23 14:00:25 +0330

    test: update test_blog_model.py

--- core/blog/tests/test_blog_model.py
+++ core/blog/tests/test_blog_model.py
@@ -6,20 +6,23 @@ from accounts.models import User, Profile
 
 class TestPostModel(TestCase):
     
-    def test_create_post_with_valid_data(self):
-        user = User.objects.create_user(email='test@test.com', password='test')
-        profile = Profile.objects.create(
-            user = user,
+    def setUp(self):
+        self.user = User.objects.create_user(email='test@test.com', password='test')
+        self.profile = Profile.objects.create(
+            user = self.user,
             first_name = 'ali',
             last_name = 'ali',
             description = 'test description'
         )
+    
+    def test_create_post_with_valid_data(self):
         post = Post.objects.create(
-            author = profile,
+            author = self.profile,
             title = "test",
             content = 'test content',
             status = True,
             category = None,
             published_at = datetime.now()
         )
+        self.asserTrue(Post.objects.filter(pk=post.pk).exists())
         self.assertEqual(post.title, "test")
