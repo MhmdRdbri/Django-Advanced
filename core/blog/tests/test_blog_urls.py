aabcbde8831b436641408477bf14929f228f1d94
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-23 12:21:10 +0330

    chore: delete tests.py

---
+++ core/blog/tests/test_blog_urls.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.urls import resolve, reverse
+from ..views import *
+
+# Create your tests here.
+
+class TestUrl(TestCase):
+    
+    def test_blog_post_list_url_resolve(self):
+        url = reverse('blog:post_list')
+        self.assertEqual(resolve(url).func.view_class,PostListView)
+        
+    def test_blog_index_url_resolve(self):
+        url = reverse('blog:post_detail',kwargs={'pk': 1})
+        self.assertEqual(resolve(url).func.view_class,PostDetailView)
