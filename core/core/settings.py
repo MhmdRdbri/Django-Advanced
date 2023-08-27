5628ad8a24ab675e9895f2d691db0aaaf2f05561
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-27 14:07:40 +0330

    build(deps): update profile.py, settings.py and requirements.txt

--- core/core/settings.py
+++ core/core/settings.py
@@ -51,12 +51,14 @@ INSTALLED_APPS = [
     "drf_yasg",
     "rest_framework_simplejwt",
     "mail_templated",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -169,3 +171,8 @@ EMAIL_HOST = "smtp4dev"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 25
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:9000",
+]
+# CORS_ALLOW_ALL_ORIGINS: bool
