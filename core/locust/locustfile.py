b832414b3cb1aaa600437dbfffd1e6c91f03f9ad
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-08-28 00:33:30 +0330

    update views.py, locustfile.py and docker-compose.yml

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,4 +1,14 @@
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    pass
+    
+    def on_start(self):
+        response = self.client.post('/accounts/api/v1/jwt/create/', data={
+            "email": "admin@gmail.com",
+            "password": "admin"
+        }).json()
+        self.client.headers = {'Authorization': f"Bearer {response.get('access', None)}"}
+    
+    @task
+    def post_list(self):
+        self.client.get("/blog/api/v1/post/")
